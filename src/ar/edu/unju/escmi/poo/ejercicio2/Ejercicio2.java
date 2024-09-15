e50cf09d90934ce26b661f44060dd5f96ee04c4e
PC <PC@MATIAS>
2024-09-15 18:37:12 -0300

    Avanze en Main de ejercicio2

--- src/ar/edu/unju/escmi/poo/ejercicio2/Ejercicio2.java
+++ src/ar/edu/unju/escmi/poo/ejercicio2/Ejercicio2.java
@@ -1,10 +1,12 @@
 package ar.edu.unju.escmi.poo.ejercicio2;
+import java.util.Scanner;
+import java.util.TreeSet;
 
 public class Ejercicio2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        TreeSet<GatoSimple> gatos = new TreeSet<>();
+        int opcion;
+}
 }
